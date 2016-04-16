4136b0c3c6015398183a6d241e97d6ffe32eaa01
Marcin Mazur <marmaz.966@o2.pl>
2016-04-16 18:31:12 +0200

    Added GuardScript.cs VIPScript.cs, DetectionIndicator.cs

---
+++ trunk/BigStarGala/Assets/Scripts/DetectionIndicator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class DetectionIndicator : MonoBehaviour
+{
+    public float Indicator { get; private set; }
+    public IndicatorState State { get; set; }
+
+    public int rate;
+
+    // Use this for initialization
+    void Start()
+    {
+        Indicator = 0;
+        State = IndicatorState.Decrement;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Debug.Log(gameObject.ToString() + "  Indicator = " + Indicator);
+        switch (State)
+            {
+                case IndicatorState.Increment:
+                    {
+                        Indicator += rate * Time.deltaTime;
+                        if (Indicator > 100)
+                            Indicator = 100;
+                    }
+                    break;
+                case IndicatorState.Decrement:
+                    {
+                        Indicator -= rate * Time.deltaTime;
+                        if (Indicator < 0)
+                            Indicator = 0;
+                    }
+                    break;
+            }
+    }
+}
+
+public enum IndicatorState
+{
+    Increment,
+    Decrement
+}
---
+++ trunk/BigStarGala/Assets/Scripts/VIPScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class VIPScript : MonoBehaviour {
+
+    private float timeSinceLastChecked;
+
+    public float CoolDown;
+    public bool IsChecked { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        timeSinceLastChecked = 0;
+        IsChecked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(IsChecked)
+            timeSinceLastChecked += Time.deltaTime;
+
+        if (timeSinceLastChecked > CoolDown)
+            IsChecked = false;
+	}
+}
