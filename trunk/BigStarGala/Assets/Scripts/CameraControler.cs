42df016189d80f1e5cf4a6a42f5b802789c0a01b
Tomek <ghost744@gmail.com>
2016-04-16 17:49:57 +0200

    Added CameraControler.cs (it moves camera along with player), added PlayerControler.cs (movement and interaction for player), added VIPController ( trigger players, if they are within their sight)

---
+++ trunk/BigStarGala/Assets/Scripts/CameraControler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControler : MonoBehaviour {
+    
+    [SerializeField]
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 offset = new Vector3(-3, 3, 0);
+        //transform.Translate(player.transform.position.x, 0 , player.transform.position.z) ;
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ trunk/BigStarGala/Assets/Scripts/VIPController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VIPController : MonoBehaviour {
+
+
+    [SerializeField]
+    private GameObject player;
+    public GameObject parent;
+    public float distance = 2f;
+    private float distanceBetweenPlayer;
+    public bool playerIsNoticed = false;
+
+
+	// Use this for initialization
+	void Start () {
+        playerIsNoticed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distanceBetweenPlayer = Vector3.Distance(transform.position, player.transform.position);
+        Debug.Log(distanceBetweenPlayer);
+
+        if(distanceBetweenPlayer <= distance && player.GetComponent<PlayerControler>().tryChangeId == true && playerIsNoticed == false)
+        {
+            player.GetComponent<PlayerControler>().changeIdentity(parent);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerIsNoticed = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerIsNoticed = false;
+        }
+    }
+    
+}
