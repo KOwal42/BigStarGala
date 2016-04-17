a209eec39860c74cbe7fe85435eaba53e540c3e4
Tomek <ghost744@gmail.com>
2016-04-17 08:33:15 +0200

    Added NPCController.cs (NPC dialogs)

---
+++ trunk/BigStarGala/Assets/NPCController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Celerity 1 is taking photos");
+    }
+}
